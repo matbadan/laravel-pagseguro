f48a6de9ca7e82f4957e2f0aaa4dbaaf3337e6e5
vanessasoutoc <vanessasoutoc@gmail.com>
2017-01-18 14:00:34 -0200

    Create PagSeguroRecorrente.php

---
+++ src/Artistas/PagSeguroRecorrente.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Artistas\PagSeguro;
+
+use Log;
+
+class PagSeguroRecorrente extends PagSeguroClient
+{
+
+    private $preApproval = [];
+
+    /**
+     * Define os dados do plano.
+     *
+     * @param array $preApproval
+     *
+     * @return $this
+     */
+    public function setPreApproval(array $preApproval){
+        $preApproval = [
+            'preApprovalName'     => $this->sanitize($preApproval, 'preApprovalName'),
+            'preApprovalCharge' => $this->sanitize($preApproval, 'preApprovalCharge'),
+            'preApprovalPeriod'    => $this->sanitize($preApproval, 'preApprovalPeriod'),
+            'preApprovalCancelUrl'    => $this->sanitize($preApproval, 'preApprovalCancelUrl'),
+            'preApprovalAmountPerPayment'    => $this->sanitize($preApproval, 'preApprovalAmountPerPayment'),
+            'preApprovalMembershipFee'    => $this->sanitize($preApproval, 'preApprovalMembershipFee'),
+            'preApprovalTrialPeriodDuration'    => $this->sanitize($preApproval, 'preApprovalTrialPeriodDuration'),
+            'preApprovalExpirationValue'    => $this->sanitize($preApproval, 'preApprovalExpirationValue'),
+            'preApprovalExpirationUnit'    => $this->sanitize($preApproval, 'preApprovalExpirationUnit'),
+            'maxUses'    => $this->sanitize($preApproval, '&maxUses'),
+        ];
+
+        $this->preApproval = $preApproval;
+        
+        Log::info($this);
+        
+        return $this;
+    }
+
+}
